0fc9ddff56bcebd19702b1a49eb2c008b39492a8
George Hazlewood <george@layer1.co.uk>
2020-06-26 08:40:22 +0100

    Updating Isotope to jQuery rather than Vanilla JS, switching to unpkg.com for JS CDN

--- assets/isotope.js
+++ assets/isotope.js
@@ -1,3 +1,18 @@
-var iso = new Isotope('.l-gallery', { itemSelector: '.gallery-item'});
-iso.layout();
-iso.shuffle();
+// Vanilla JS Isotope
+//var iso = new Isotope('.l-gallery', { itemSelector: '.gallery-item'});
+//iso.layout();
+//iso.shuffle();
+
+// jQuery version of Isotope
+var $=jQuery.noConflict();
+$(document).ready(function(){
+	// init Isotope
+	var $grid = $('.l-gallery').isotope({
+	  itemSelector: '.gallery-item'
+	});
+	// layout Isotope after each image loads
+	$grid.imagesLoaded().progress( function() {
+	  $grid.isotope('layout');
+	});
+	$grid.isotope('shuffle');
+});
